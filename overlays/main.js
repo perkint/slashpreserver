afa881383636929cf4739edde8f8a4d27de27f82
perkint <github23@noid.net>
2015-07-01 16:37:39 -0700

    subsequent commit

--- overlays/main.js
+++ overlays/main.js
@@ -17,8 +17,32 @@ var myThingy = {
   init: function() {
     window.addEventListener("keypress", function(event) {
       var target = event.originalTarget;
-      if ( target instanceof HTMLTextAreaElement ||
+      //
+      // You can test slashpreserver on an editable div here:
+      //
+      //   http://html5doctor.com/the-contenteditable-attribute/
+      //
+      // If testing for attribute contentEditable == 'true' starts to catch
+      // too many things (?), one can limit it to various tags like so:
+      //
+      //   && (/DIV|BLOCKQOTE/).test(target.tagName)
+      //
+      // Testing for contentEditable may be sufficient.  If not, here are some
+      // other attributes of interest:
+      //
+      //   g_editable="true"  <--  google specific
+      //
+      //   spellcheck="true"  <--  generaly used
+      //
+      //   role="textbox"     <--  generaly used
+      //   role="combobox"    <--  this one is text related too
+      //
+      //     http://www.w3.org/TR/wai-aria/roles#role_definitions
+      //
+      if ( target.contentEditable == 'true'      ||
+           target instanceof HTMLTextAreaElement ||
           (target instanceof HTMLInputElement && target.type == "text") ) {
+
         // no-op: allow slash to be typed into input fields, etc...
       } else {
         if ( event.charCode ) {
